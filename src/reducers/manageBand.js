7929263dac4d538ce4d3e967fd813a7c9279ff62
GuttermanA <alexanderf.gutterman@gmail.com>
2018-03-29 15:27:47 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,16 @@
+let id = 0;
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
+      let band = {...action.band, id: ++id}
+      return { bands: state.bands.concat(band) }
+      break;
+    case 'DELETE_BAND':
+      return { bands: state.bands.filter(band => band.id !== action.id)}
+      break;
     default:
       return state;
   }
