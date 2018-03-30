aee07f0aeaffa98f6a6e0080c1647f9491637ae6
TorreJohnson <torre.johnson@gmail.com>
2018-03-30 12:01:00 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Band extends Component {
-  render() {
-    return(
-      <div>
-        band component
-      </div>
-    );
-  }
-};
+	handleClick = () => {
+		this.props.store.dispatch({
+			type: "DELETE_BAND",
+			id: this.props.band.id
+		});
+	};
+	render() {
+		return (
+			<div>
+				<li>{this.props.band.text}</li>
+				<button onClick={this.handleClick}>Delete</button>
+			</div>
+		);
+	}
+}
 
 export default Band;
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,18 @@
-export default function manageBand(state = {
-  bands: []
-}, action) {
-  switch (action.type) {
-    case 'ADD_BAND':
-      return { bands: state.bands.concat(action.band) }
-    default:
-      return state;
-  }
-};
+let id = 0;
+
+export default function manageBand(
+	state = {
+		bands: []
+	},
+	action
+) {
+	switch (action.type) {
+		case "ADD_BAND":
+			return { bands: [...state.bands, { text: action.band.text, id: ++id }] };
+		case "DELETE_BAND":
+			const filteredBands = state.bands.filter(band => band.id !== action.id);
+			return { bands: filteredBands };
+		default:
+			return state;
+	}
+}
